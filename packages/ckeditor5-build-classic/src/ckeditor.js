8e999cb10b5bbdb7e7fcadcf491ebbd3763ae45b
evyjeanett <42544219+evyjeanett@users.noreply.github.com>
2022-11-09 21:44:40 +0100

    Update imports
    Co-authored-by: Tim <hilhorstt@users.noreply.github.com>

--- packages/ckeditor5-build-classic/src/ckeditor.js
+++ packages/ckeditor5-build-classic/src/ckeditor.js
@@ -37,10 +37,10 @@ import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformatio
 import CloudServices from '@ckeditor/ckeditor5-cloud-services/src/cloudservices';
 import Style from '@ckeditor/ckeditor5-style/src/style';
 import SpecialCharacters from '@ckeditor/ckeditor5-special-characters/src/specialcharacters';
-import SpecialCharactersCurrency from './specialcharacterscurrency';
-import SpecialCharactersLatin from './specialcharacterslatin';
-import SpecialCharactersMathematical from './specialcharactersmathematical';
-import SpecialCharactersText from './specialcharacterstext';
+import SpecialCharactersCurrency from '@ckeditor/ckeditor5-special-characters/src/specialcharacterscurrency';
+import SpecialCharactersLatin from '@ckeditor/ckeditor5-special-characters/src/specialcharacterslatin';
+import SpecialCharactersMathematical from '@ckeditor/ckeditor5-special-characters/src/specialcharactersmathematical';
+import SpecialCharactersText from '@ckeditor/ckeditor5-special-characters/src/specialcharacterstext';
 
 class ClassicEditor extends ClassicEditorBase {}
 class InlineEditor extends InlineEditorBase {}
