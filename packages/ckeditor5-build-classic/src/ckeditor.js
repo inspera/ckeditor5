783ae571bbd4a8b28caa2f1739d109752b13c8f6
evyjeanett <42544219+evyjeanett@users.noreply.github.com>
2022-11-10 12:41:47 +0100

    removing SpecialCharactersEssentials (#5)

--- packages/ckeditor5-build-classic/src/ckeditor.js
+++ packages/ckeditor5-build-classic/src/ckeditor.js
@@ -37,17 +37,10 @@ import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformatio
 import CloudServices from '@ckeditor/ckeditor5-cloud-services/src/cloudservices';
 import Style from '@ckeditor/ckeditor5-style/src/style';
 import SpecialCharacters from '@ckeditor/ckeditor5-special-characters/src/specialcharacters';
-import SpecialCharactersCurrency from '@ckeditor/ckeditor5-special-characters/src/specialcharacterscurrency';
-import SpecialCharactersLatin from '@ckeditor/ckeditor5-special-characters/src/specialcharacterslatin';
-import SpecialCharactersMathematical from '@ckeditor/ckeditor5-special-characters/src/specialcharactersmathematical';
-import SpecialCharactersText from '@ckeditor/ckeditor5-special-characters/src/specialcharacterstext';
 
 class ClassicEditor extends ClassicEditorBase {}
 class InlineEditor extends InlineEditorBase {}
 
-const SpecialCharactersEssentials = [
-	SpecialCharactersCurrency, SpecialCharactersLatin, SpecialCharactersMathematical, SpecialCharactersText
-];
 // Plugins to include in the build.
 const plugins = [
 	Essentials,
@@ -74,7 +67,6 @@ const plugins = [
 	Paragraph,
 	PasteFromOffice,
 	SpecialCharacters,
-	SpecialCharactersEssentials,
 	Style,
 	Subscript,
 	Superscript,
